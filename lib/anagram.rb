c968e8f62c8949789eeba740c05ef15fc3a868e5
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-10 18:09:44 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  #attr_accessor :allAnagrams, :characters
+  attr_accessor :some_word
+  def initialize(some_word)
+    @some_word = some_word
+  end
+
+  def match(list)
+    list.each_with_object([]) do |word, matching|
+      if word.split("").sort == self.some_word.split("").sort
+        matching << word
+      end
+    end
+  end
+
+
+end
